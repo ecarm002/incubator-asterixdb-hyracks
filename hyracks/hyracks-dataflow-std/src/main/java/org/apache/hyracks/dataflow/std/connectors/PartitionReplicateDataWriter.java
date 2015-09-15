fb8d8a52f9e95d9ff981f9ee823b370a87f77f2c
Preston Carman <prestonc@apache.org>
2015-09-15 16:42:50 -0700

    apache header update.

--- hyracks/hyracks-dataflow-std/src/main/java/org/apache/hyracks/dataflow/std/connectors/PartitionReplicateDataWriter.java
+++ hyracks/hyracks-dataflow-std/src/main/java/org/apache/hyracks/dataflow/std/connectors/PartitionReplicateDataWriter.java
@@ -1,16 +1,20 @@
 /*
- * Copyright 2009-2013 by The Regents of the University of California
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * you may obtain a copy of the License from
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.apache.hyracks.dataflow.std.connectors;
 
@@ -27,9 +31,9 @@ public class PartitionReplicateDataWriter extends AbstractPartitionDataWriter {
     private final ITuplePartitionReplicatorComputer tprc;
 
     public PartitionReplicateDataWriter(IHyracksTaskContext ctx, int consumerPartitionCount,
-            IPartitionWriterFactory pwFactory, RecordDescriptor recordDescriptor, ITuplePartitionReplicatorComputer tprc)
-            throws HyracksDataException {
-        super(ctx, consumerPartitionCount, pwFactory,recordDescriptor);
+            IPartitionWriterFactory pwFactory, RecordDescriptor recordDescriptor,
+            ITuplePartitionReplicatorComputer tprc) throws HyracksDataException {
+        super(ctx, consumerPartitionCount, pwFactory, recordDescriptor);
         this.tprc = tprc;
     }
 
